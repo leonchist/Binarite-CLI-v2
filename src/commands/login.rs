4bebc2099f25746c1d395e6b51418c9bbfd7e9c2
leonchrist <am000hw@gmail.com>
2024-06-17 13:49:12 +0200

    Initial code for cli, implementing login, logout, sphere creation/listing/deletion

---
+++ src/commands/login.rs
@@ -0,0 +1,33 @@
+use cred_store::CredStore;
+
+use super::CommandContext;
+use crate::auth;
+
+fn save_tokens<T: CredStore>(
+    access_token: &str,
+    refresh_token: &str,
+    context: &mut CommandContext<T>,
+) -> Result<(), std::io::Error> {
+    context
+        .cred_store
+        .clear()
+        .add("access_token".to_string(), access_token.to_string())
+        .add("refresh_token".to_string(), refresh_token.to_string())
+        .save()
+}
+
+pub async fn login<'a, T: CredStore>(context: &mut CommandContext<'a, T>) {
+    match auth::login(context.config).await {
+        Ok(resp) => {
+            let access_token = resp.access_token.clone().unwrap();
+            let refresh_token = resp.refresh_token.clone().unwrap_or_default();
+            println!();
+            println!("Access Token: {}", access_token);
+            if save_tokens(&access_token, &refresh_token, context).is_err() {
+                eprintln!("Couldn't configure credentials.");
+                std::process::exit(1);
+            }
+        }
+        Err(e) => println!("Error logging in: {}", e),
+    }
+}
---
+++ src/commands/mod.rs
@@ -0,0 +1,96 @@
+mod context;
+mod login;
+mod logout;
+mod sphere;
+
+use clap::{Args, Parser, Subcommand};
+pub use context::CommandContext;
+use login::login;
+use logout::logout;
+use openapi::apis::configuration::Configuration;
+
+use cred_store::CredStore;
+
+use crate::auth::get_token::get_token;
+
+use self::sphere::{
+    create_sphere, delete_sphere, list_sphere, SphereCreateArgs, SphereDeleteArgs, SphereListArgs,
+};
+
+#[derive(Parser)]
+#[clap(author, version, about = "A MetaGravity command line tool")]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Login,
+    Logout,
+    Sphere(SphereArgs),
+    Project,
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+struct SphereArgs {
+    #[command(subcommand)]
+    command: SphereCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SphereCommands {
+    Create(SphereCreateArgs),
+    Delete(SphereDeleteArgs),
+    List(SphereListArgs),
+    Status,
+    Output,
+}
+
+pub async fn parse_command<'a, T: CredStore>(context: &mut CommandContext<'a, T>) {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Login => login(context).await,
+        Command::Logout => logout(context),
+        _ => {}
+    }
+
+    let access_token = match get_token(context) {
+        Ok(token) => match token {
+            Some(token) => token,
+            None => {
+                eprintln!("You must login first.");
+                std::process::exit(1);
+            }
+        },
+        Err(e) => {
+            eprintln!("Couldn't get credentials: {}.  Try to login again.", e);
+            std::process::exit(1);
+        }
+    };
+
+    let mut configuration = Configuration::new();
+
+    configuration.bearer_access_token = Some(access_token);
+    configuration.base_path = context.config.binarite_url.to_string();
+
+    match cli.command {
+        Command::Sphere(sphere) => match sphere.command {
+            SphereCommands::Create(args) => {
+                create_sphere(&configuration, args).await;
+            }
+            SphereCommands::Delete(args) => {
+                delete_sphere(&configuration, args).await;
+            }
+            SphereCommands::List(args) => {
+                list_sphere(&configuration, &args).await;
+            }
+            SphereCommands::Status => todo!(),
+            SphereCommands::Output => todo!(),
+        },
+        Command::Project => todo!(),
+        _ => {}
+    }
+}
