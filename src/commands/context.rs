6ed168010ff66baa2a16b9a5b85358fa17e656ee
leonchrist <am000hw@gmail.com>
2024-06-17 13:48:11 +0200

    Remove code for code review

--- src/commands/context.rs
+++
@@ -1,8 +0,0 @@
-use crate::config::Config;
-use cred_store::CredStore;
-
-#[derive(Debug)]
-pub struct CommandContext<'a, T: CredStore> {
-    pub config: &'a Config,
-    pub cred_store: &'a mut T,
-}
